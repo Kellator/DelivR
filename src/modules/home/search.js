75dd511c2e62657789c73bab890a6f0a6480e1c6
Kellator <k.macleod21@gmail.com>
2017-03-18 16:47:25 -0400

    reintegreated semantic-ui-react formating for search page

--- src/modules/home/search.js
+++ src/modules/home/search.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import {Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
+import { Grid, Form } from 'semantic-ui-react';
 
 const Search = (props) => {
     const { handleSubmit, pristine, reset, submitting } = props
     return (
-        <form onSubmit={handleSubmit} className="search-form">
-            <div >
-                <label>I'm Hungry For...</label>
-                <Field name="cuisine" component="input" type="text" placeholder="Chinese" />
-            </div>
-            <div>
-                <label>Location</label>
-                <Field name="location" component="input" type="text" placeholder="City, State or Zipcode" />
-            </div>
-            <button type="submit" disabled={pristine || submitting}>Feed Me!</button>
-            <button type="button" disabled={pristine || submitting} onClick={reset}>Changed my mind...</button>
-        </form>
+        <Grid centered >
+            <Grid.Column width={6}>
+                <Form>
+                <form onSubmit={handleSubmit} className="search-form">
+                    <Form.Field>
+                        <div >
+                            <label>I'm Hungry For...</label>
+                            <Field name="cuisine" component="input" type="text" placeholder="Chinese" />
+                        </div>
+                    </Form.Field>
+                    <Form.Field>
+                        <div>
+                            <label>Location</label>
+                            <Field name="location" component="input" type="text" placeholder="City, State or Zipcode" />
+                        </div>
+                    </Form.Field>
+                    <button type="submit" disabled={pristine || submitting}>Feed Me!</button>
+                    <button type="button" disabled={pristine || submitting} onClick={reset}>Changed my mind...</button>
+                </form>
+                </Form>
+            </Grid.Column>
+        </Grid>
     )
 }
 Search = reduxForm({
