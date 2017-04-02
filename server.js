b5e571a7f5e234a1cafb5af07440d83dddb1b4d4
Kellator <k.macleod21@gmail.com>
2017-04-02 14:27:28 -0400

    added cors module.  response object now returned.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var cors = require('cors');
+app.use(cors());
 
 // const tokenUrl = "https://api.yelp.com/oauth2/token?client_id=F1GjwxdHmDgOyEQnFkrOdg&client_secret=6VupwbF7anbAd8yHZYbl9CDDQDFzzmORu2al1E3JkqaI1HSvGEqFSLT6M8VDpLZp&grant_type=client_credentials"; 
 const tokenUrl = 'https://api.yelp.com/oauth2/token';
 
  
 // client app calls server and server makes a request to the remote resource.
-app.post('/login', function(req, res) {
+app.post('/login', function(req, res, next) {
+    res.json({msg: 'this is cors-enabled for all origins'})
     var url = tokenUrl;
     request(url, {
         'headers' : {            
@@ -19,7 +22,9 @@ app.post('/login', function(req, res) {
     ).pipe(res);
     console.log(req);
 });
-app.listen(process.env.PORT || 3030);
+app.listen((process.env.PORT || 3030), function () {
+    console.log('CORS-enabled web server listening on port 3030')
+});
 console.log('server started')
 
 // 'Content-Type' : 'application/x-www-form-urlencoded',
