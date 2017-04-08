2b97064a72af355f30d1effd40cf7c8e2a1d9f97
Kellator <k.macleod21@gmail.com>
2017-04-08 19:14:32 -0400

    star rating working

--- src/modules/home/result.js
+++ src/modules/home/result.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import StarRater from './StarRater';
+import '../../index.css';
 
 const Result = (props) => {
         console.log(props);
         return (
             <div className="result-item">
                 <li>
-                    <p>Name: {props.result.name}</p>
-                    <p>Phone: {props.result.display_phone}</p>
-                    <p>hello can you see me?</p>
+                    <img className="result-image" src={props.result.image_url} />
+                    <h2><a href={props.result.url}>Name: {props.result.name}</a></h2>
+                    <h3>Phone: {props.result.display_phone}</h3>
+                    <h3>Rating: </h3><StarRater rating={props.result.rating} />
                 </li>
             </div>
         );
