c3282004415717dd950d1656712031d233a3aad6
Kellator <k.macleod21@gmail.com>
2017-04-15 20:12:23 -0400

    added grid-cell to result list item

--- src/modules/home/result.js
+++ src/modules/home/result.js
@@ -6,7 +6,7 @@ import '../../index.css';
 const Result = (props) => {
         console.log(props);
         return (         
-                <li className="result-item rounded Grid--fit">                   
+                <li className="result-item rounded Grid-cell">                   
                     <img className="result-image " src={props.result.image_url} />
                     <h2 className="result-text Grid--1of4 "><a className="link-color" href={props.result.url}>{props.result.name}</a></h2>
                     <h3 className="result-text Grid--1of4 "><a className="phone-link" href="tel:{props.result.display_phone}">{props.result.display_phone}</a></h3>
