f8a5cea07b51ebdc098f83b1a2a2a4696f49e51e
Kellator <k.macleod21@gmail.com>
2017-04-16 17:36:48 -0400

    nav bar restyled without using Semantic-ui

--- src/modules/home/HomeView.js
+++ src/modules/home/HomeView.js
@@ -8,8 +8,9 @@ const HomeView = () => (
     <div className="Grid Grid-column search-div">
         <div className="Grid-cell">
             <div className="Grid Grid-column ">
-                <h1 className="title Grid-cell Grid-cell--center title"> DelivR </h1>   
-                <p className=" home-text Grid-cell Grid-cell--center"> Hungry?  Nothing in the kitchen?  Don't want to go out? </p>
+                <h1 className="Grid-cell Grid-cell--center title">DelivR</h1>   
+                <p className="home-text Grid-cell Grid-cell--center">Hungry?  Nothing in the kitchen?</p>
+                <p className="home-text Grid-cell Grid-cell--center">Don't want to go out?</p>
                 <p className="home-text Grid-cell Grid-cell--center ">Let DelivR find you dinner that delivers!</p> 
             </div>
         </div>
--- src/modules/layout/NavBar.js
+++ src/modules/layout/NavBar.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
 import { browserHistory } from 'react-router';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = ({ path }) => (
-	<div className="Grid-cell" >
-		<div className="Grid  menu">
-			<div className="Grid-cell Grid-cell--top ">
-				<ul>
-					<li><Link to='/'>Home</Link></li>
-				</ul>
-			</div>	
-			<div className="Grid-cell Grid-cell--bottom">
-				<ul>
-					<li><Link to='/about'>About</Link></li>
-					<li><Link to='/more'>More</Link></li>
-				</ul>
-			</div>
-		</div>
+	<div className="Grid-cell Grid-cell--center Grid--fit menu  " >
+		<ul className="Grid navigation">
+			<li className="Grid-cell Grid-cell--center nav-list float-left"><NavLink activeClassName="selected" to='/'>Home</NavLink></li>
+			<li className="Grid-cell Grid-cell--center nav-list float-right"><NavLink  activeClassName="selected" to='/about'>About</NavLink></li>
+			<li className="Grid-cell Grid-cell--center nav-list float-right"><NavLink  activeClassName="selected" to='/more'>More</NavLink></li>
+		</ul>
 	</div>
 );
 
