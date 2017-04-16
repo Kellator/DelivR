23229f525716c1888468056a8515cfe52c0637b8
Kellator <k.macleod21@gmail.com>
2017-04-16 19:28:38 -0400

    button restyled for mobile

--- src/modules/home/search.js
+++ src/modules/home/search.js
@@ -8,16 +8,16 @@ const Search = (props) => {
     return (
         <div className="Grid Grid-column ">
             <form onSubmit={handleSubmit(props.onSubmit)} className="Grid Grid-column search-div" >
-                    <div className="Grid-cell Grid-cell--center bottom-space">
-                        <label className="home-text Grid-cell label-flex">I'm Hungry For...</label>
-                        <Field className="block rounded" name="cuisine" component="input" type="text" placeholder="Chinese" />
-                    </div>
-                    <div className="Grid-cell Grid-cell--center space-small">
-                        <label className="home-text Grid-cell label-flex">Location</label>
-                        <Field className="block rounded " name="location" required component="input" type="text" placeholder="City, State or Zipcode" />
-                    </div>
+                <div className="Grid-cell Grid-cell--center bottom-space">
+                    <label className="home-text Grid-cell label-flex">I'm Hungry For...</label>
+                    <Field className="block rounded" name="cuisine" component="input" type="text" placeholder="Chinese" />
+                </div>
                 <div className="Grid-cell Grid-cell--center space-small">
-                    <button className="Grid-cell Grid-cell--center rounded button " type="submit" disabled={pristine || submitting}>Feed Me!</button>
+                    <label className="home-text Grid-cell label-flex">Location</label>
+                    <Field className="block rounded " name="location" required component="input" type="text" placeholder="City, State or Zipcode" />
+                </div>
+                <div className="Grid-cell Grid-cell--center space-small">
+                    <button className="Grid-cell Grid-cell--center block rounded button " type="submit" disabled={pristine || submitting}>Feed Me!</button>
                 </div>
                 {/*<button className="block rounded button flex-item" type="button" disabled={pristine || submitting} onClick={reset}>Changed my mind...</button>*/}
             </form>
