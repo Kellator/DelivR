0f04ab248589aa7a2d350f9b8caefa2649caaaf2
Kellator <k.macleod21@gmail.com>
2017-04-25 22:14:07 -0400

    added alt image

--- src/modules/home/result.js
+++ src/modules/home/result.js
@@ -7,7 +7,7 @@ const Result = (props) => {
         console.log(props);
         return (         
                 <li className="col_4 result_item" >                   
-                        <img className=" result-image" src={props.result.image_url} />
+                        <img className="result-image" alt="image from restaraunt" src={props.result.image_url} />
                         <h2 className="center-text"><a className="link-color" href={props.result.url}>{props.result.name}</a></h2>
                         <h3 className="center-text"><a className="phone-link" href="tel:{props.result.display_phone}">{props.result.display_phone}</a></h3>
                         <StarRater  rating={props.result.rating} />                    
