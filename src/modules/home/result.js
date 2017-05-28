29815fce593bc6b6c6dec010fd2268941826e077
Kellator <k.macleod21@gmail.com>
2017-05-27 20:12:47 -0400

    Results cards have centered contents and images have rounded borders

--- src/modules/home/result.js
+++ src/modules/home/result.js
@@ -6,7 +6,7 @@ import '../../index.css';
 const Result = (props) => {
         return (         
                 <li className="col_4 result_card" >   
-                        <div className="row">             
+                        <div className="row card_contents">             
                                 <img className="result-image " alt="food" src={props.result.image_url} />
                                 <h2 className="center-text"><a className="link-color" href={props.result.url}>{props.result.name}</a></h2>
                                 <h3 className="center-text"><a className="phone-link" href={`tel:${props.result.phone}`}>{props.result.display_phone}</a></h3>
